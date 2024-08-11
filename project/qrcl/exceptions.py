49c6e7769fd553b71e395ecad02f2bd3445a3bbb
KOSASIH <kosasihg88@gmail.com>
2024-08-11 08:29:40 +0700

    Create exceptions.py

---
+++ project/qrcl/exceptions.py
@@ -0,0 +1,5 @@
+# qrcl/exceptions.py
+
+from .utils.errors import *
+
+__all__ = ["QRCLException", "KeyGenerationError", "EncryptionError", "DecryptionError", "InvalidKeyError", "InvalidMessageError", "CryptographyError"]

0e5ff880bf3eb41751a52f3f4d0c79ea9004f3a7
KOSASIH <kosasihg88@gmail.com>
2024-08-11 09:31:52 +0700

    Create __init__.py

---
+++ project/qrcl/tests/__init__.py
@@ -0,0 +1,7 @@
+# tests/__init__.py
+
+from .test_algorithms import *
+from .test_key_agreement import *
+from .test_digital_signatures import *
+
+__all__ = ["TestAlgorithms", "TestKeyAgreement", "TestDigitalSignatures"]

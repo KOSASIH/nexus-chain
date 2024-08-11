88f559635c6dfe880e43ac425fd52129034a94b6
KOSASIH <kosasihg88@gmail.com>
2024-08-11 08:44:45 +0700

    Update ntru.py

--- project/qrcl/code_based/ntru.py
+++ project/qrcl/code_based/ntru.py
@@ -19,4 +19,30 @@ class NTRU:
         c = self._encrypt(m, h)
         return c
 
-    def decrypt(self, c: np.array, f: np.array) -> np
+    def decrypt(self, c: np.array, f: np.array) -> np.array:
+        m = self._decrypt(c, f)
+        return m
+
+    def _generate_secret_key(self) -> np.array:
+        # Generate a random secret key f of size n
+        f = np.random.randint(0, self.p, size=self.n)
+        return f
+
+    def _generate_public_key(self, f: np.array) -> np.array:
+        # Generate a public key h of size n
+        h = np.random.randint(0, self.q, size=self.n)
+        return h
+
+    def _encrypt(self, m: np.array, h: np.array) -> np.array:
+        # Encrypt the message m using the public key h
+        c = np.convolve(m, h) % self.q
+        # Add random errors to the ciphertext
+        e = np.random.randint(0, self.q, size=self.n)
+        c += e
+        return c
+
+    def _decrypt(self, c: np.array, f: np.array) -> np.array:
+        # Decrypt the ciphertext c using the secret key f
+        # Use the convolution theorem to recover the message
+        m = np.convolve(c, f) % self.p
+        return m

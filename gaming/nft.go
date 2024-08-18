bed9f194ac02f14ee788c09c94ff414c6718c23c
KOSASIH <kosasihg88@gmail.com>
2024-08-18 11:50:58 +0700

    Create nft.go

---
+++ gaming/nft.go
@@ -0,0 +1,49 @@
+package gaming
+
+import (
+	"github.com/opensea/opensea-go"
+	"github.com/rarible/rarible-go"
+)
+
+// NFTMarketplace represents an NFT marketplace
+type NFTMarketplace struct {
+	opensea *opensea.Client
+	rarible *rarible.Client
+}
+
+// NewNFTMarketplace returns a new instance of NFTMarketplace
+func NewNFTMarketplace(openseaAPIKey string, raribleAPIKey string) (*NFTMarketplace, error) {
+	openseaClient, err := opensea.NewClient(openseaAPIKey)
+	if err != nil {
+		return nil, err
+	}
+
+	raribleClient, err := rarible.NewClient(raribleAPIKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &NFTMarketplace{
+		opensea: openseaClient,
+		rarible: raribleClient,
+	}, nil
+}
+
+// CreateNFT creates a new NFT
+func (nm *NFTMarketplace) CreateNFT(name string, description string, image string) (*NFT, error) {
+	var nft *NFT
+	var err error
+
+	if nm.opensea != nil {
+		nft, err = nm.opensea.CreateNFT(name, description, image)
+	} else if nm.rarible != nil {
+		nft, err = nm.rarible.CreateNFT(name, description, image)
+	} else {
+		return nil, fmt.Errorf("no NFT marketplace available")
+	}
+
+	return nft, err
+}
+
+// GetNFT returns an NFT by ID
+func (nm *NFTMarketplace) GetNFT(id string)

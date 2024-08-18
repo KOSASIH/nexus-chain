3b07795ce3d1501fa93ee8a1f96ab20b7a470248
KOSASIH <kosasihg88@gmail.com>
2024-08-18 11:53:06 +0700

    Update nft.go

--- gaming/nft.go
+++ gaming/nft.go
@@ -46,4 +46,55 @@ func (nm *NFTMarketplace) CreateNFT(name string, description string, image strin
 }
 
 // GetNFT returns an NFT by ID
-func (nm *NFTMarketplace) GetNFT(id string)
+func (nm *NFTMarketplace) GetNFT(id string) (*NFT, error) {
+	var nft *NFT
+	var err error
+
+	if nm.opensea != nil {
+		nft, err = nm.opensea.GetNFT(id)
+	} else if nm.rarible != nil {
+		nft, err = nm.rarible.GetNFT(id)
+	} else {
+		return nil, fmt.Errorf("no NFT marketplace available")
+	}
+
+	return nft, err
+}
+
+// UpdateNFT updates an NFT
+func (nm *NFTMarketplace) UpdateNFT(id string, name string, description string, image string) (*NFT, error) {
+	var nft *NFT
+	var err error
+
+	if nm.opensea != nil {
+		nft, err = nm.opensea.UpdateNFT(id, name, description, image)
+	} else if nm.rarible != nil {
+		nft, err = nm.rarible.UpdateNFT(id, name, description, image)
+	} else {
+		return nil, fmt.Errorf("no NFT marketplace available")
+	}
+
+	return nft, err
+}
+
+// DeleteNFT deletes an NFT
+func (nm *NFTMarketplace) DeleteNFT(id string) error {
+	var err error
+
+	if nm.opensea != nil {
+		err = nm.opensea.DeleteNFT(id)
+	} else if nm.rarible != nil {
+		err = nm.rarible.DeleteNFT(id)
+	} else {
+		return fmt.Errorf("no NFT marketplace available")
+	}
+
+	return err
+}
+
+type NFT struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+}

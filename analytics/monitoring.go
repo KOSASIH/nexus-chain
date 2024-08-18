bebcff09f28d35da477cd2a6f597a92993bb0c96
KOSASIH <kosasihg88@gmail.com>
2024-08-18 11:46:39 +0700

    Update monitoring.go

--- analytics/monitoring.go
+++ analytics/monitoring.go
@@ -38,4 +38,47 @@ func (mc *MonitoringClient) RegisterMetric(metric *prometheus.GaugeVec) error {
 }
 
 // Incr increments a metric
-func (mc *MonitoringClient) Incr(metric *prometheus.GaugeVec, labels prometheus.Label
+func (mc *MonitoringClient) Incr(metric *prometheus.GaugeVec, labels prometheus.Labels) {
+	metric.With(labels).Inc()
+}
+
+// Decr decrements a metric
+func (mc *MonitoringClient) Decr(metric *prometheus.GaugeVec, labels prometheus.Labels) {
+	metric.With(labels).Dec()
+}
+
+// Set sets a metric value
+func (mc *MonitoringClient) Set(metric *prometheus.GaugeVec, labels prometheus.Labels, value float64) {
+	metric.With(labels).Set(value)
+}
+
+// GetMetric gets a metric value
+func (mc *MonitoringClient) GetMetric(metric *prometheus.GaugeVec, labels prometheus.Labels) (float64, error) {
+	return metric.With(labels).Get(), nil
+}
+
+// NewGaugeVec creates a new gauge vector
+func (mc *MonitoringClient) NewGaugeVec(name string, help string, labels []string) (*prometheus.GaugeVec, error) {
+	return prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: name,
+		Help: help,
+	}, labels)
+}
+
+// NewCounterVec creates a new counter vector
+func (mc *MonitoringClient) NewCounterVec(name string, help string, labels []string) (*prometheus.CounterVec, error) {
+	return prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: name,
+		Help: help,
+	}, labels)
+}
+
+// NewSummaryVec creates a new summary vector
+func (mc *MonitoringClient) NewSummaryVec(name string, help string, labels []string, obj *prometheus.SummaryOpts) (*prometheus.SummaryVec, error) {
+	return prometheus.NewSummaryVec(obj, labels)
+}
+
+// NewHistogramVec creates a new histogram vector
+func (mc *MonitoringClient) NewHistogramVec(name string, help string, labels []string, obj *prometheus.HistogramOpts) (*prometheus.HistogramVec, error) {
+	return prometheus.NewHistogramVec(obj, labels)
+}
